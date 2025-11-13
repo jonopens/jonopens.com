a48a7eec172762cb2f500d3c5500d8923c7505dc
Jon Openshaw <10333387+jonopens@users.noreply.github.com>
2025-11-12 20:55:15 -0500

    removes remark-collapse and unused util

--- src/utils/slugify.ts
+++
@@ -1,5 +0,0 @@
-import { slug as githubSlug } from "github-slugger";
-
-export const slugifyStr = (str: string) => githubSlug(str);
-
-export const slugifyAll = (arr: string[]) => arr.map((str) => slugifyStr(str));
